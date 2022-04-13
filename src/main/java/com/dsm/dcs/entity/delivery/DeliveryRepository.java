48a545ed62739d797b93e2f438a4c8b7099dd6bc
hee <cmlgml094356@gmail.com>
2022-04-13 23:01:57 +0900

    🌱 :: DeliveryRepository

---
+++ src/main/java/com/dsm/dcs/entity/delivery/DeliveryRepository.java
@@ -0,0 +1,7 @@
+package com.dsm.dcs.entity.delivery;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
+
+}
