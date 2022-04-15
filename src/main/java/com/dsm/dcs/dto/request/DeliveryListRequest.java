ea5348b8a848f15c63b54a00d0d0b27aad922979
hee <cmlgml094356@gmail.com>
2022-04-15 17:08:54 +0900

    🌱 :: 택배 정보 request

---
+++ src/main/java/com/dsm/dcs/dto/request/DeliveryListRequest.java
@@ -0,0 +1,43 @@
+package com.dsm.dcs.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DeliveryListRequest {
+
+    @NotBlank
+    private String courierCompany;
+
+    List<DeliveryDto> delivery = new ArrayList<>();
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public class DeliveryDto {
+
+        @NotBlank
+        @Size(max = 10)
+        private String recipientName;
+
+        @NotBlank
+        @Size(max = 11)
+        private String recipientPhoneNumber;
+
+        @NotBlank
+        @Size(max = 30)
+        private String product;
+
+    }
+
+}
+
+
