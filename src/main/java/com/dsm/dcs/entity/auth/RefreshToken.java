8cf646d21df938da1c59c4bfb1b6dbdea5cfbb79
Sangjin <sangjin170202@naver.com>
2022-05-16 17:57:43 +0900

    🌱 :: RefreshToken

---
+++ src/main/java/com/dsm/dcs/entity/auth/RefreshToken.java
@@ -0,0 +1,34 @@
+package com.dsm.dcs.entity.auth;
+
+import lombok.*;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@RedisHash
+public class RefreshToken implements Serializable {
+
+    @Id
+    private String accountId;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long refreshExp;
+
+    public void updateToken(String token, Long refreshExp) {
+
+        this.token = token;
+        this.refreshExp = refreshExp;
+    }
+
+}
