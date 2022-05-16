dfeeb8e2dc58b9dca80fd841441631c13815ac35
Sangjin <sangjin170202@naver.com>
2022-05-16 18:27:25 +0900

    🌼 :: RefreshToken

--- src/main/java/com/dsm/dcs/entity/auth/RefreshToken.java
+++ src/main/java/com/dsm/dcs/entity/auth/RefreshToken.java
@@ -1,7 +1,10 @@
 package com.dsm.dcs.entity.auth;
 
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
