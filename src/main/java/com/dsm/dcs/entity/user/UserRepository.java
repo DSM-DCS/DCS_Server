4f4b7306a8210e2e747cf0fd2cf0c6d353c92a26
Sangjin <sangjin170202@naver.com>
2022-05-16 17:59:34 +0900

    🌼 :: UserRepository

--- src/main/java/com/dsm/dcs/entity/user/UserRepository.java
+++ src/main/java/com/dsm/dcs/entity/user/UserRepository.java
@@ -1,11 +1,12 @@
 package com.dsm.dcs.entity.user;
 
-import com.dsm.dcs.entity.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    Optional<User> findById(String accountId);
+    Optional<User> findByAccountId(String accountId);
+    Optional<User> findByStudentNumber(Integer studentNumber);
+    Optional<User> findByEmail(String email);
 }
