f2e3b3b29a1b86edef85ee3306f4d452130093cf
hee <cmlgml094356@gmail.com>
2022-05-19 14:50:28 +0900

    🌼 ::  택배회사 필드 enum타입으로 수정

--- src/main/java/com/dsm/dcs/entity/delivery/Delivery.java
+++ src/main/java/com/dsm/dcs/entity/delivery/Delivery.java
@@ -1,6 +1,7 @@
 package com.dsm.dcs.entity.delivery;
 
 import com.dsm.dcs.entity.BaseTimeEntity;
+import com.dsm.dcs.entity.CourierCompany;
 import com.dsm.dcs.entity.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,10 +10,11 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import java.math.BigInteger;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,14 +22,15 @@ import java.math.BigInteger;
 public class Delivery extends BaseTimeEntity {
 
     @Column(nullable = false)
-    private BigInteger courierCompany;
+    @Enumerated(EnumType.STRING)
+    private CourierCompany courierCompany;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
-    public Delivery(Long id, BigInteger courierCompany, User user) {
+    public Delivery(Long id, CourierCompany courierCompany, User user) {
         this.id = id;
         this.courierCompany = courierCompany;
         this.user = user;
