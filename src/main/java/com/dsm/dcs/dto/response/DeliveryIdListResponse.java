9387421a6961f448c6b89fd36019700c88c867de
hee <cmlgml094356@gmail.com>
2022-05-20 18:47:14 +0900

    🌼 ::  response에 Getter 어노테이션 추가

--- src/main/java/com/dsm/dcs/dto/response/DeliveryIdListResponse.java
+++ src/main/java/com/dsm/dcs/dto/response/DeliveryIdListResponse.java
@@ -1,14 +1,17 @@
 package com.dsm.dcs.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class DeliveryIdListResponse {
 
     private final List<DeliveryIdResponse> deliveryIdResponses;
 
+    @Getter
     public static class DeliveryIdResponse{
 
         private final Long deliveryId;
