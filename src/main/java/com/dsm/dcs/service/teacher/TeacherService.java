3c99be50d10f145fe49102f16838345366a52873
hee <cmlgml094356@gmail.com>
2022-05-23 20:08:31 +0900

    🌱 :: TeacherService 생성

---
+++ src/main/java/com/dsm/dcs/service/teacher/TeacherService.java
@@ -0,0 +1,12 @@
+package com.dsm.dcs.service.teacher;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherService {
+
+
+
+}
