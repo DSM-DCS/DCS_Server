bdfc7e6e8367ef2be5bbae88650ecdc36a0d9615
hee <cmlgml094356@gmail.com>
2022-05-26 19:51:31 +0900

    🌼 :: service 택배 목록 가져오기

--- src/main/java/com/dsm/dcs/service/teacher/TeacherService.java
+++ src/main/java/com/dsm/dcs/service/teacher/TeacherService.java
@@ -1,6 +1,8 @@
 package com.dsm.dcs.service.teacher;
 
 import com.dsm.dcs.dto.response.DeliveryIdListResponse;
+import com.dsm.dcs.dto.response.DeliveryListResponse;
+import com.dsm.dcs.entity.CourierCompany;
 import com.dsm.dcs.entity.delivery.Delivery;
 import com.dsm.dcs.entity.delivery.DeliveryRepository;
 import com.dsm.dcs.entity.user.User;
@@ -9,6 +11,9 @@ import com.dsm.dcs.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -31,6 +36,27 @@ public class TeacherService {
 
     public void deleteDelivery(Long deliveryId) {
         deliveryFacade.deleteDelivery(deliveryId);
+    }
+
+    public DeliveryListResponse getDeliveryList() {
+
+        List<DeliveryListResponse.DeliveryResponse> deliveryResponses = new ArrayList<>();
+        List<Delivery> deliveries = deliveryFacade.getDeliveryList();
+
+        for(Delivery delivery : deliveries) {
+            if(delivery.getUser() != null) {
+                deliveryResponses.add(
+                        DeliveryListResponse.DeliveryResponse.builder()
+                                .courierCompany(delivery.getCourierCompany().name())
+                                .name(delivery.getUser().getName())
+                                .createdDate(delivery.getCreatedDate())
+                                .build()
+                );
+            }
+        }
+
+        return new DeliveryListResponse(deliveryResponses);
+
     }
 
 }
