5cd50e6821a36344ff093ac0090ccd7daee540c1
hee <cmlgml094356@gmail.com>
2022-05-26 18:51:55 +0900

    🌼 :: controller user 정보 불러오기

--- src/main/java/com/dsm/dcs/controller/UserController.java
+++ src/main/java/com/dsm/dcs/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.dsm.dcs.controller;
 
 import com.dsm.dcs.dto.request.UserSignUpRequest;
+import com.dsm.dcs.dto.response.UserListResponse;
 import com.dsm.dcs.dto.response.UserTokenResponse;
+import com.dsm.dcs.service.user.UserService;
 import com.dsm.dcs.service.user.UserSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,13 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserSignUpService userSignUpService;
+    private final UserService userService;
+
+    @GetMapping
+    @ResponseStatus
+    public UserListResponse getUser() {
+        return userService.getUser();
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
