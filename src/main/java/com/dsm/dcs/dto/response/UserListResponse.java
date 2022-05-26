8fdb1d44baba4f370da5221e9bbb609045f85255
hee <cmlgml094356@gmail.com>
2022-05-26 18:50:42 +0900

    🌱 :: user response 생성

---
+++ src/main/java/com/dsm/dcs/dto/response/UserListResponse.java
@@ -0,0 +1,30 @@
+package com.dsm.dcs.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UserListResponse {
+
+    private List<UserResponse> userResponses;
+
+    @Getter
+    @Builder
+    public class UserResponse {
+
+        private final String name;
+        private final Integer studentNumber;
+
+        public UserResponse(String name, Integer studentNumber) {
+            this.name = name;
+            this.studentNumber = studentNumber;
+        }
+
+    }
+
+
+}
