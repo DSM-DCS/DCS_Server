43360680a241bccd685cacaff95cbda5deb342e2
Sangjin <sangjin170202@naver.com>
2022-05-30 18:46:21 +0900

    🌼 :: UserController

--- src/main/java/com/dsm/dcs/controller/UserController.java
+++ src/main/java/com/dsm/dcs/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.dsm.dcs.controller;
 
+import com.dsm.dcs.dto.request.SendEmailRequest;
+import com.dsm.dcs.dto.request.UpdatePasswordRequest;
 import com.dsm.dcs.dto.request.UserSignUpRequest;
+import com.dsm.dcs.dto.request.VerificationAuthCodeRequest;
+import com.dsm.dcs.dto.response.UserAccountIdResponse;
 import com.dsm.dcs.dto.response.UserListResponse;
 import com.dsm.dcs.dto.response.UserTokenResponse;
-import com.dsm.dcs.service.user.UserService;
-import com.dsm.dcs.service.user.UserSignUpService;
+import com.dsm.dcs.service.user.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,6 +23,10 @@ public class UserController {
 
     private final UserSignUpService userSignUpService;
     private final UserService userService;
+    private final SendEmailAuthCodeService sendEmailAuthCodeService;
+    private final SearchAccountIdService searchAccountIdService;
+    private final UpdatePasswordService updatePasswordService;
+    private final VerificationAuthCodeService verificationAuthCodeService;
 
     @GetMapping("/search")
     @ResponseStatus
@@ -43,6 +44,28 @@ public class UserController {
     @PostMapping
     public UserTokenResponse userSignUp(@RequestBody @Valid UserSignUpRequest request) {
         return userSignUpService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/email-verifications")
+    public void sendEmailAuthCode(@RequestBody @Valid SendEmailRequest request) {
+        sendEmailAuthCodeService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/password")
+    public void updatePassword(@RequestBody @Valid UpdatePasswordRequest request) {
+        updatePasswordService.execute(request);
+    }
+
+    @GetMapping("/accounts/{email}")
+    public UserAccountIdResponse searchAccountId(@PathVariable(name = "email") String email) {
+        return searchAccountIdService.execute(email);
+    }
+
+    @PutMapping("/email-verifications")
+    public void verifyEmail(@RequestBody @Valid VerificationAuthCodeRequest request) {
+        verificationAuthCodeService.execute(request);
     }
 
 }
