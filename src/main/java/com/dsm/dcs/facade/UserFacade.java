37ff585a719997f6dc863a22999746384794c26e
Sangjin <sangjin170202@naver.com>
2022-05-30 18:35:32 +0900

    🌱 :: UserFacade

--- src/main/java/com/dsm/dcs/facade/UserFacade.java
+++ src/main/java/com/dsm/dcs/facade/UserFacade.java
@@ -1,11 +1,10 @@
 package com.dsm.dcs.facade;
 
 import com.dsm.dcs.entity.user.User;
+import com.dsm.dcs.entity.user.UserAuthCode;
+import com.dsm.dcs.entity.user.UserAuthCodeRepository;
 import com.dsm.dcs.entity.user.UserRepository;
-import com.dsm.dcs.exception.AccountIdAlreadyExistsException;
-import com.dsm.dcs.exception.EmailAlreadyExistsException;
-import com.dsm.dcs.exception.StudentNumberAlreadyExistsException;
-import com.dsm.dcs.exception.UserNotFoundException;
+import com.dsm.dcs.exception.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,18 +16,19 @@ import java.util.List;
 public class UserFacade {
 
     private final UserRepository userRepository;
+    private final UserAuthCodeRepository userAuthCodeRepository;
 
     public User getCurrentUser() {
         String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
         return getUserByAccountId(accountId);
     }
 
-    public List<User> getUsetList() {
+    public List<User> getUserList() {
         return userRepository.findAllByOrderByStudentNumberDesc();
     }
 
-    public User getUserByAccountId(String id) {
-        return userRepository.findByAccountId(id)
+    public User getUserByAccountId(String accountId) {
+        return userRepository.findByAccountId(accountId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 
@@ -44,6 +44,13 @@ public class UserFacade {
     public User getUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public boolean isAlreadyExists(String email) {
+        if(userRepository.findByEmail(email).isPresent()) {
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+        return true;
     }
 
     public void checkUserExists(String accountId) {
@@ -63,4 +70,40 @@ public class UserFacade {
             throw EmailAlreadyExistsException.EXCEPTION;
         }
     }
+
+    public void checkPhoneNumberExists(String phoneNumber) {
+        if (userRepository.findByPhoneNumber(phoneNumber).isPresent()) {
+            throw PhoneNumberAlreadyExistsException.EXCEPTION;
+        }
+    }
+
+    public boolean checkVerified(boolean isVerified) {
+        if (!isVerified) {
+            throw UnVerifiedAuthCodeException.EXCEPTION;
+        }
+
+        return true;
+    }
+
+    public boolean isVerified(String email) {
+        return getAuthCodeById(email).isVerify();
+    }
+
+    public UserAuthCode getAuthCodeById(String email) {
+        return userAuthCodeRepository.findById(email)
+                .orElseThrow(() -> InvalidAuthCodeException.EXCEPTION);
+    }
+
+    public boolean isAlreadyVerified(boolean isVerified) {
+        if (isVerified) {
+            throw AuthCodeAlreadyVerifiedException.EXCEPTION;
+        }
+
+        return true;
+    }
+
+    public boolean compareCode(String reqCode, String code) {
+        return reqCode.equals(code);
+    }
+
 }
