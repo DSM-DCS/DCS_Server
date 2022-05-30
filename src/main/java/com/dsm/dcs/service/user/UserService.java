5f5de205eaa3e32c98e6a9e38107d1a7a6835254
hee <cmlgml094356@gmail.com>
2022-05-30 21:35:59 +0900

    🌼 :: 자신의 택배 목록 조회 service

--- src/main/java/com/dsm/dcs/service/user/UserService.java
+++ src/main/java/com/dsm/dcs/service/user/UserService.java
@@ -1,7 +1,10 @@
 package com.dsm.dcs.service.user;
 
+import com.dsm.dcs.dto.response.DeliveryListResponse;
 import com.dsm.dcs.dto.response.UserListResponse;
+import com.dsm.dcs.entity.delivery.Delivery;
 import com.dsm.dcs.entity.user.User;
+import com.dsm.dcs.facade.DeliveryFacade;
 import com.dsm.dcs.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ import java.util.List;
 public class UserService {
 
     private final UserFacade userFacade;
+    private final DeliveryFacade deliveryFacade;
 
     public UserListResponse getUser() {
         List<User> userList = userFacade.getUserList();
@@ -25,6 +29,23 @@ public class UserService {
     public UserListResponse searchUser(String name) {
         List<User> userList = userFacade.getUserByName(name);
         return getUserList(userList);
+    }
+
+    public DeliveryListResponse getDeliveryList(Long userId) {
+        List<DeliveryListResponse.DeliveryResponse> deliveryResponses = new ArrayList<>();
+        List<Delivery> deliveries = deliveryFacade.getDeliveryList(userFacade.getCurrentUser());
+
+        for(Delivery delivery : deliveries) {
+            deliveryResponses.add(
+                    DeliveryListResponse.DeliveryResponse.builder()
+                            .courierCompany(delivery.getCourierCompany().name())
+                            .name(delivery.getUser().getName())
+                            .createdDate(delivery.getCreatedDate())
+                            .build()
+            );
+        }
+
+        return new DeliveryListResponse(deliveryResponses);
     }
 
     private UserListResponse getUserList(List<User> userList) {
