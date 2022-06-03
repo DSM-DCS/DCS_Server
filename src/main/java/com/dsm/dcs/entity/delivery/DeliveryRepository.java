7a3a3fe6541494039f12e03ad20a105bec4188c3
hee <cmlgml094356@gmail.com>
2022-06-03 09:39:11 +0900

    🌼 :: DeliveryRepository 페이징처리

--- src/main/java/com/dsm/dcs/entity/delivery/DeliveryRepository.java
+++ src/main/java/com/dsm/dcs/entity/delivery/DeliveryRepository.java
@@ -1,10 +1,11 @@
 package com.dsm.dcs.entity.delivery;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
-    List<Delivery> findAllByOrderByIdDesc();
-    List<Delivery> findByUserOrderByCreatedDateDesc();
+    List<Delivery> findAllByOrderByIdDesc(Pageable page);
+    List<Delivery> findByUserOrderByCreatedDateDesc(Pageable page);
 }
