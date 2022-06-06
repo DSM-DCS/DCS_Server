e2b9431bfb1794a7fd67e3ce7cdac67b2da18991
Sangjin <sangjin170202@naver.com>
2022-06-06 15:05:55 +0900

    🌼 :: DeliveryRepository

--- src/main/java/com/dsm/dcs/entity/delivery/DeliveryRepository.java
+++ src/main/java/com/dsm/dcs/entity/delivery/DeliveryRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
     List<Delivery> findAllByOrderByIdDesc();
-    List<Delivery> findByUserOrderByCreatedDateDesc();
+//    List<Delivery> findByUserOrderByCreatedDateDesc();
 }
