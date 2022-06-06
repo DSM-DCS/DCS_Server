9d9e9ac7c9e6f8dc4314038742775929fc00df3f
Sangjin <sangjin170202@naver.com>
2022-06-06 15:03:49 +0900

    🌱 :: CourierSignUpRequest

---
+++ src/main/java/com/dsm/dcs/dto/request/CourierSignUpRequest.java
@@ -0,0 +1,27 @@
+package com.dsm.dcs.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CourierSignUpRequest {
+
+    @NotBlank(message = "account_id는 null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Size(min = 6, max = 20, message = "account_id는 6글자 이상, 20글자 이하여야 합니다.")
+    private String accountId;
+
+    @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
+            "<=>?@＼^_`{|}~]{8,30}$",
+            message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다.")
+    private String password;
+
+}
