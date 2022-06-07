f844257dc2c187a4c5ff9529b205ae8a7e1cd262
hee <cmlgml094356@gmail.com>
2022-06-07 23:27:32 +0900

    🌱 :: DeliveryService 생성

---
+++ src/main/java/com/dsm/dcs/service/delivery/DeliveryService.java
@@ -0,0 +1,28 @@
+package com.dsm.dcs.service.delivery;
+
+import com.dsm.dcs.dto.request.DeliveryListRequest;
+import com.dsm.dcs.dto.response.DeliveryIdListResponse;
+import com.dsm.dcs.dto.response.DeliveryListResponse;
+import com.dsm.dcs.dto.response.DeliveryNullUserListResponse;
+import com.dsm.dcs.entity.CourierCompany;
+import com.dsm.dcs.entity.delivery.Delivery;
+import com.dsm.dcs.entity.delivery.DeliveryRepository;
+import com.dsm.dcs.entity.user.User;
+import com.dsm.dcs.facade.DeliveryFacade;
+import com.dsm.dcs.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class DeliveryService {
+
+    
+
+}
