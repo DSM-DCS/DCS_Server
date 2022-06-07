6e91c6dbef0712260cdf5ea11bf49f1612c0b868
hee <cmlgml094356@gmail.com>
2022-06-07 23:40:06 +0900

    ♻️ :: User이 Null이 아닌 택배 목록 가져오기 Facade

--- src/main/java/com/dsm/dcs/facade/DeliveryFacade.java
+++ src/main/java/com/dsm/dcs/facade/DeliveryFacade.java
@@ -1,5 +1,6 @@
 package com.dsm.dcs.facade;
 
+import com.dsm.dcs.dto.response.DeliveryListResponse;
 import com.dsm.dcs.dto.response.DeliveryNullUserListResponse;
 import com.dsm.dcs.entity.delivery.Delivery;
 import com.dsm.dcs.entity.delivery.DeliveryRepository;
@@ -27,9 +28,22 @@ public class DeliveryFacade {
         deliveryRepository.delete(delivery);
     }
 
+    public DeliveryListResponse getDeliveryUserNotNullList(Pageable page) {
+        return new DeliveryListResponse(deliveryRepository.findAllByUserNotNullOrderByCreatedDateDesc(page)
+                .stream().map(this::getDelivery).collect(Collectors.toList()));
+    }
+
     public DeliveryNullUserListResponse getDeliveryUserNullList(Pageable page) {
         return new DeliveryNullUserListResponse(deliveryRepository.findAllByUserOrderByCreatedDateDesc(null, page)
                 .stream().map(this::getUserNullDeilvery).collect(Collectors.toList()));
+    }
+
+    private DeliveryListResponse.DeliveryResponse getDelivery(Delivery delivery) {
+        return DeliveryListResponse.DeliveryResponse.builder()
+                .courierCompany(delivery.getCourierCompany().name())
+                .name(delivery.getUser().getName())
+                .createdDate(delivery.getCreatedDate())
+                .build();
     }
 
     private DeliveryNullUserListResponse.DeliveryNullUserResponse getUserNullDeilvery(Delivery delivery) {
