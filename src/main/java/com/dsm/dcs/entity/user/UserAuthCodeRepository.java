e427701706061d3b3b0e6eefb18f375fa5c0322c
Sangjin <sangjin170202@naver.com>
2022-06-10 16:52:39 +0900

    🌼 :: UserAuthCodeRepository

--- src/main/java/com/dsm/dcs/entity/user/UserAuthCodeRepository.java
+++ src/main/java/com/dsm/dcs/entity/user/UserAuthCodeRepository.java
@@ -1,7 +1,6 @@
 package com.dsm.dcs.entity.user;
 
 import com.dsm.dcs.entity.Action;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
