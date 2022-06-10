55bcc5c2cd436c4e9d5ec887f2cf65b316037b98
Sangjin <sangjin170202@naver.com>
2022-06-10 13:23:12 +0900

    🌼 :: RedisConfig

--- src/main/java/com/dsm/dcs/redis/RedisConfig.java
+++ src/main/java/com/dsm/dcs/redis/RedisConfig.java
@@ -5,19 +5,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
-import java.time.Duration;
-
-
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
@@ -25,18 +18,9 @@ public class RedisConfig {
     private final RedisProperties redisProperties;
 
     @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig =
-                new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-        if(redisProperties.getPassword() != null && !redisProperties.getPassword().isBlank())
-            redisConfig.setPassword(redisProperties.getPassword());
-        
-        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
-                .commandTimeout(Duration.ofSeconds(3))
-                .shutdownTimeout(Duration.ZERO)
-                .build();
-
-        return new LettuceConnectionFactory(redisConfig, clientConfiguration);
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
 
     @Bean
@@ -47,5 +31,10 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(RedisSerializer.string());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations() {
+        return redisTemplate().opsForZSet();
     }
 }
