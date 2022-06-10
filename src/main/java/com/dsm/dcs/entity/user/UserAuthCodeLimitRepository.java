f58a10b2f3ac3c2a6b9ddfd8821c1dab80ebf4d6
Sangjin <sangjin170202@naver.com>
2022-06-10 16:51:30 +0900

    🌼 :: UserAuthCodeLimitRepository

--- src/main/java/com/dsm/dcs/entity/user/UserAuthCodeLimitRepository.java
+++ src/main/java/com/dsm/dcs/entity/user/UserAuthCodeLimitRepository.java
@@ -1,8 +1,6 @@
 package com.dsm.dcs.entity.user;
 
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
-@EnableRedisRepositories
 public interface UserAuthCodeLimitRepository extends CrudRepository<UserAuthCodeLimit, String> {
 }
