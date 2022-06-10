f2565045ec9610ab408978da89cd024d6ec89803
Sangjin <sangjin170202@naver.com>
2022-06-10 16:47:31 +0900

    🌼 :: UserAuthCodeRepository

--- src/main/java/com/dsm/dcs/entity/user/UserAuthCodeRepository.java
+++ src/main/java/com/dsm/dcs/entity/user/UserAuthCodeRepository.java
@@ -1,10 +1,12 @@
 package com.dsm.dcs.entity.user;
 
 import com.dsm.dcs.entity.Action;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+@EnableRedisRepositories
 public interface UserAuthCodeRepository extends CrudRepository<UserAuthCode, String> {
 
     Optional<UserAuthCode> findByEmailAndAction(String email, Action action);
