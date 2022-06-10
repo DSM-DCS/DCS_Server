1318649cd61dcc5aa910286396bf61d2973428fe
hee <cmlgml094356@gmail.com>
2022-06-10 22:32:32 +0900

    🌱 :: TokenDto 생성

---
+++ src/main/java/com/dsm/dcs/dto/TokenDto.java
@@ -0,0 +1,18 @@
+package com.dsm.dcs.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TokenDto {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    @Builder
+    public TokenDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+}
