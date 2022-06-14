69a698140b6b11af9561ae0e1f569f174f3a3789
hee <cmlgml094356@gmail.com>
2022-06-14 12:19:05 +0900

    🌼 :: Admin Facade로 합침
    Teacher과 Courier로 나뉘어 있던 로직을 admin으로 합침

---
+++ src/main/java/com/dsm/dcs/facade/AdminFacade.java
@@ -0,0 +1,27 @@
+package com.dsm.dcs.facade;
+
+import com.dsm.dcs.entity.admin.Admin;
+import com.dsm.dcs.entity.admin.AdminRepository;
+import com.dsm.dcs.exception.AccountIdAlreadyExistsException;
+import com.dsm.dcs.exception.AdminNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminFacade {
+
+    private final AdminRepository adminRepository;
+
+    public Admin findByAdminId(String adminId) {
+        return adminRepository.findByAdminId(adminId)
+                .orElseThrow(() -> AdminNotFoundException.EXCEPTION);
+    }
+
+    public void checkUserExists(String accountId) {
+        if (adminRepository.findByAdminId(accountId).isPresent()) {
+            throw AccountIdAlreadyExistsException.EXCEPTION;
+        }
+    }
+
+}
