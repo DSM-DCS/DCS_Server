01b6b1d4557fa1ffa382ea21a0e643f6b7c436f5
hee <cmlgml094356@gmail.com>
2022-06-16 17:11:31 +0900

    🌼 :: 생성자 추가

--- src/main/java/com/dsm/dcs/entity/post/Post.java
+++ src/main/java/com/dsm/dcs/entity/post/Post.java
@@ -2,6 +2,7 @@ package com.dsm.dcs.entity.post;
 
 import com.dsm.dcs.entity.BaseTimeEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,5 +19,11 @@ public class Post extends BaseTimeEntity {
 
     @Column(nullable = false, length = 500)
     private String content;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
 }
