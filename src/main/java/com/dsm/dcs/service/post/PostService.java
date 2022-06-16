d522086f60056fdbcdf9594380c4e5ca2d468e9a
hee <cmlgml094356@gmail.com>
2022-06-16 15:55:04 +0900

    🌱 :: Service 생성

---
+++ src/main/java/com/dsm/dcs/service/post/PostService.java
@@ -0,0 +1,15 @@
+package com.dsm.dcs.service.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class PostService {
+
+
+
+
+}
