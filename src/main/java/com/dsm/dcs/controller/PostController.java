973b40df61680458c0fece55cd751a4fe8b8f0a9
hee <cmlgml094356@gmail.com>
2022-06-16 17:13:17 +0900

    🌼 :: 공지 게시글 저장 기능 controller 구현

--- src/main/java/com/dsm/dcs/controller/PostController.java
+++ src/main/java/com/dsm/dcs/controller/PostController.java
@@ -1,15 +1,29 @@
 package com.dsm.dcs.controller;
 
+import com.dsm.dcs.dto.request.PostRequest;
+import com.dsm.dcs.dto.response.PostIdResponse;
+import com.dsm.dcs.service.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/post")
 @RestController
 public class PostController {
 
+    private final PostService postService;
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostIdResponse savePost(@Valid @RequestBody PostRequest request) {
+        return postService.savePost(request);
+    }
 
 }
