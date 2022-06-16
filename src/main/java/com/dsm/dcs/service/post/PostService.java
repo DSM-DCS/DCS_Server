3ae35efd6abf079e6ab51ebe23e65ca23bc1223c
hee <cmlgml094356@gmail.com>
2022-06-16 17:13:01 +0900

    🌼 :: 공지 게시글 저장 기능 service 구현

--- src/main/java/com/dsm/dcs/service/post/PostService.java
+++ src/main/java/com/dsm/dcs/service/post/PostService.java
@@ -1,5 +1,9 @@
 package com.dsm.dcs.service.post;
 
+import com.dsm.dcs.dto.request.PostRequest;
+import com.dsm.dcs.dto.response.PostIdResponse;
+import com.dsm.dcs.entity.post.Post;
+import com.dsm.dcs.entity.post.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,7 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PostService {
 
+    private final PostRepository postRepository;
 
+    public PostIdResponse savePost(PostRequest request) {
+        return new PostIdResponse(
+                postRepository.save(
+                        Post.builder()
+                                .title(request.getTitle())
+                                .content(request.getContent())
+                                .build()
+                ).getId()
+        );
+    }
 
 
 }
