9b28639a548ef29b82f0612a4af0ce35b5126e02
hee <cmlgml094356@gmail.com>
2022-06-16 16:02:44 +0900

    🌼 ::Entity 필드 생성

--- src/main/java/com/dsm/dcs/entity/post/Post.java
+++ src/main/java/com/dsm/dcs/entity/post/Post.java
@@ -1,15 +1,22 @@
 package com.dsm.dcs.entity.post;
 
+import com.dsm.dcs.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Post {
+public class Post extends BaseTimeEntity {
 
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
 
 }
