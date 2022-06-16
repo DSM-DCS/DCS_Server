e3d914355ce54a3728d2ac8a7ee87bbf810ddbbd
hee <cmlgml094356@gmail.com>
2022-06-16 15:09:15 +0900

    🌾 :: 사용하지 않는 import문 삭제

--- src/main/java/com/dsm/dcs/service/user/UserService.java
+++ src/main/java/com/dsm/dcs/service/user/UserService.java
@@ -1,15 +1,11 @@
 package com.dsm.dcs.service.user;
 
 import com.dsm.dcs.dto.response.UserListResponse;
-import com.dsm.dcs.entity.user.User;
 import com.dsm.dcs.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional
