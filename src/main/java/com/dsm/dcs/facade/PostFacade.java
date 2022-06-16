12a515f298e6ece266e25c2202abac7e93d3ed80
hee <cmlgml094356@gmail.com>
2022-06-16 23:38:26 +0900

    🌼 :: id로 post 찾기

--- src/main/java/com/dsm/dcs/facade/PostFacade.java
+++ src/main/java/com/dsm/dcs/facade/PostFacade.java
@@ -1,6 +1,8 @@
 package com.dsm.dcs.facade;
 
+import com.dsm.dcs.entity.post.Post;
 import com.dsm.dcs.entity.post.PostRepository;
+import com.dsm.dcs.exception.PostNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,9 @@ public class PostFacade {
 
     private final PostRepository postRepository;
 
-
+    public Post findById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> PostNotFoundException.EXCEPTION);
+    }
 
 }
