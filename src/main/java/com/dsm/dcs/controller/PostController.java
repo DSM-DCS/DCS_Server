79eeb03375b12d8953b05b286abe6b7ce8ad622a
hee <cmlgml094356@gmail.com>
2022-06-16 15:39:47 +0900

    🌼 :: 이름 변경

--- src/main/java/com/dsm/dcs/controller/PostController.java
+++ src/main/java/com/dsm/dcs/controller/PostController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/notice")
+@RequestMapping("/post")
 @RestController
-public class NoticeController {
+public class PostController {
 
 
 
