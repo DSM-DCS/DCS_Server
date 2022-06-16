636c44cd5dc6a936f6ea27f72875bd81c04b3eaa
hee <cmlgml094356@gmail.com>
2022-06-16 23:24:39 +0900

    🌱 :: FireBaseContig 생성

---
+++ src/main/java/com/dsm/dcs/firebase/FireBaseInitialize.java
@@ -0,0 +1,34 @@
+package com.dsm.dcs.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configurable
+public class FireBaseConfig {
+
+    @Value("${app.firebase-file}")
+    private String FIREBASE_CONFIG_PATH;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            ClassPathResource resource = new ClassPathResource(FIREBASE_CONFIG_PATH);
+            InputStream serviceAccount = resource.getInputStream();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+        }
+    }
+
+}
