8784f29c7f697a1ce5cffc59d4e8da7441c26a86
hee <cmlgml094356@gmail.com>
2022-06-16 15:54:33 +0900

    🌱 :: Entity, Repository 생성

---
+++ src/main/java/com/dsm/dcs/entity/post/Post.java
@@ -0,0 +1,15 @@
+package com.dsm.dcs.entity.post;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post {
+
+
+}
