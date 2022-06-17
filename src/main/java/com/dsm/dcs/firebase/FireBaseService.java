3b48612f3e2b0cbcc49bab254f1ea080b64f16f3
hee <cmlgml094356@gmail.com>
2022-06-17 10:48:04 +0900

    🌼 :: firebase 메세지 전송 기능 코드 구현

---
+++ src/main/java/com/dsm/dcs/firebase/FireBaseService.java
@@ -0,0 +1,77 @@
+package com.dsm.dcs.firebase;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class FireBaseService {
+
+    @Value("${app.firebase-url}")
+    private String API_URL;
+
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
+
+        FireBaseMessage fcmMessage = FireBaseMessage.builder()
+                .message(FireBaseMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FireBaseMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        )
+                        .build()
+                )
+                .validateOnly(false)
+                .build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+
+    }
+
+
+    private String getAccessToken() throws IOException {
+        String firebaseConfigPath = "firebase/cocotalk_firebase_service_key.json";
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+}
