bd68ac8923d96ea6987dc7ce0df7681d461dcef5
hee <cmlgml094356@gmail.com>
2022-06-17 10:44:43 +0900

    🌼 :: 클래스 이름 변경

--- src/main/java/com/dsm/dcs/firebase/FireBaseMessage.java
+++ src/main/java/com/dsm/dcs/firebase/FireBaseMessage.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 @Getter
-public class FcmMessage {
+public class FireBaseMessage {
     private boolean validateOnly;
     private Message message;
 
