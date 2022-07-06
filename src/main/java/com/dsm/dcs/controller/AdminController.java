caafb635bc2b34d424c194e4af26e2aaa45f3466
hee <cmlgml094356@gmail.com>
2022-07-06 22:27:40 +0900

    🌼 :: Role이 Courier인지 확인 api controller

--- src/main/java/com/dsm/dcs/controller/AdminController.java
+++ src/main/java/com/dsm/dcs/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.dsm.dcs.service.admin.AdminAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,6 +32,11 @@ public class AdminController {
     @GetMapping("/verification/teacher")
     public Boolean verificationTeacher() {
         return adminAuthService.verificationTeacher();
+    }
+
+    @GetMapping("/verification/courier")
+    public Boolean verificationCourier() {
+        return adminAuthService.verificationCourier();
     }
 
 }
