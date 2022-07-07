be2efb92c51aa8aaf956f69f01789e9c0d46c9a6
hee <cmlgml094356@gmail.com>
2022-07-07 20:10:11 +0900

    🌱 :: DeviceToken Entity, Repository

---
+++ src/main/java/com/dsm/dcs/entity/deviceToken/DeviceToken.java
@@ -0,0 +1,29 @@
+package com.dsm.dcs.entity.deviceToken;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class DeviceToken {
+
+    @Id
+    private String accountId;
+
+    @Column(nullable = false)
+    private String deviceToken;
+
+    @Builder
+    public DeviceToken(String accountId, String deviceToken) {
+        this.accountId = accountId;
+        this.deviceToken = deviceToken;
+    }
+
+}
