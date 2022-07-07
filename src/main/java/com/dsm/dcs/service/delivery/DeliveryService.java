1634e1c8f1a1dc387f24a57a1cdce0fd47edbab0
hee <cmlgml094356@gmail.com>
2022-07-07 20:11:54 +0900

    🌼 :: 택배기사가 택배 등록 시 알림 기능 추가

--- src/main/java/com/dsm/dcs/service/delivery/DeliveryService.java
+++ src/main/java/com/dsm/dcs/service/delivery/DeliveryService.java
@@ -8,13 +8,18 @@ import com.dsm.dcs.entity.CourierCompany;
 import com.dsm.dcs.entity.delivery.Delivery;
 import com.dsm.dcs.entity.delivery.DeliveryRepository;
 import com.dsm.dcs.entity.user.User;
+import com.dsm.dcs.exception.FireBaseException;
+import com.dsm.dcs.exception.handler.DcsException;
 import com.dsm.dcs.facade.DeliveryFacade;
+import com.dsm.dcs.facade.DeviceTokenFacade;
 import com.dsm.dcs.facade.UserFacade;
+import com.dsm.dcs.firebase.FireBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +29,9 @@ import java.util.List;
 public class DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
+    private final FireBaseService fireBaseService;
     private final DeliveryFacade deliveryFacade;
+    private final DeviceTokenFacade deviceTokenFacade;
     private final UserFacade userFacade;
 
     public DeliveryIdListResponse saveDelivery(DeliveryListRequest request) {
@@ -32,17 +39,23 @@ public class DeliveryService {
         List<DeliveryIdListResponse.DeliveryIdResponse> deliveryIdResponses = new ArrayList<>();
 
         for (DeliveryListRequest.PhoneNumberRequest phoneNumberRequest : request.getPhoneNumberRequestList()) {
+            User user = userFacade.getUserByPhoneNumber(phoneNumberRequest.getPhoneNumber());
             deliveryIdResponses.add(
                     new DeliveryIdListResponse.DeliveryIdResponse(
                             deliveryRepository.save(
                                     Delivery.builder()
                                             .courierCompany(CourierCompany.valueOf(request.getCouriercompany()))
                                             .phoneNumber(phoneNumberRequest.getPhoneNumber())
-                                            .user(userFacade.getUserByPhoneNumber(phoneNumberRequest.getPhoneNumber()))
+                                            .user(user)
                                             .build()
                             ).getId()
                     )
             );
+            try {
+                fireBaseService.sendMessageTo(deviceTokenFacade.findByDeviceToken(user.getAccountId()).getDeviceToken(), "DCS", "택배가 기숙사로 배송이 완료되었습니다.");
+            } catch (IOException e) {
+                throw FireBaseException.EXCEPTION;
+            }
         }
 
         return new DeliveryIdListResponse(deliveryIdResponses);
