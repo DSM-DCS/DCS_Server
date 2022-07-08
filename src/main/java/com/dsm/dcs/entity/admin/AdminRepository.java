38b9ae24d5a6b94e0b0f1f29a64ae544687eea67
hee <cmlgml094356@gmail.com>
2022-07-09 00:43:21 +0900

    🌾 :: Admin Entity 삭제
    - 관련된 클래스 삭제

--- src/main/java/com/dsm/dcs/entity/admin/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dsm.dcs.entity.admin;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface AdminRepository extends CrudRepository<Admin, Long> {
-    Optional<Admin> findByAdminId(String adminId);
-}
