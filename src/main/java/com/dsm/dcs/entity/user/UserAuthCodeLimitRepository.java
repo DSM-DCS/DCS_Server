f68d36a654391c27a5de1005d1cc6a055cfdcb88
hee <cmlgml094356@gmail.com>
2022-07-08 11:45:51 +0900

    🌾 :: 이메일 인증 Entity 삭제

--- src/main/java/com/dsm/dcs/entity/user/UserAuthCodeLimitRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dsm.dcs.entity.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserAuthCodeLimitRepository extends CrudRepository<UserAuthCodeLimit, String> {
-}
