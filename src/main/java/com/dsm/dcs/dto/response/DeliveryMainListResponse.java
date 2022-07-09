5eb37fd3f663f71b3c2ba60aae272e8e3f59aa1b
hee <cmlgml094356@gmail.com>
2022-07-10 01:57:33 +0900

    🌼 :: 택배 전체 목록 Response

---
+++ src/main/java/com/dsm/dcs/dto/response/DeliveryMainListResponse.java
@@ -0,0 +1,34 @@
+package com.dsm.dcs.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DeliveryMainListResponse {
+
+    private List<DeliveryResponse> deliveryResponses;
+
+    @Getter
+    public static class DeliveryResponse {
+
+        private final Long id;
+        private final String courierCompany;
+        private final String products;
+        private final LocalDate createdDate;
+
+        @Builder
+        public DeliveryResponse(Long id, String courierCompany, String products, LocalDate createdDate) {
+            this.id = id;
+            this.courierCompany = courierCompany;
+            this.products = products;
+            this.createdDate = createdDate;
+        }
+
+    }
+
+}
