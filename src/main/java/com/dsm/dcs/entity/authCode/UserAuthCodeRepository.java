5a4c42f3f1ef54a5981eff11466f52e4f2bd9665
Sangjin <sangjin170202@naver.com>
2022-07-10 20:44:40 +0900

    🌱 :: UserAuthCodeRepository

---
+++ src/main/java/com/dsm/dcs/entity/authCode/UserAuthCodeRepository.java
@@ -0,0 +1,6 @@
+package com.dsm.dcs.entity.authCode;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserAuthCodeRepository extends CrudRepository<UserAuthCode, String> {
+}
