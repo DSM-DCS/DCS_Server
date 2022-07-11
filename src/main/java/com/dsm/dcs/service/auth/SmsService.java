aca2a3157d7edc1c0c7933043e97c4222e9f3834
hee <cmlgml094356@gmail.com>
2022-07-11 10:01:31 +0900

    🌱 :: SmsService
    - 입력된 핸드폰 번호로 인증번호 발송
    - 입력된 핸드폰 번호로 전송된 인증번호 검증

---
+++ src/main/java/com/dsm/dcs/service/auth/SmsService.java
@@ -0,0 +1,37 @@
+package com.dsm.dcs.service.auth;
+
+import com.dsm.dcs.dto.request.PhoneNumberRequest;
+import com.dsm.dcs.dto.request.SmsRequest;
+import com.dsm.dcs.entity.authCode.UserAuthCode;
+import com.dsm.dcs.entity.authCode.UserAuthCodeRepository;
+import com.dsm.dcs.exception.PhoneNumberNotFoundException;
+import com.dsm.dcs.exception.PhoneNumberNotMatchException;
+import com.dsm.dcs.sms.SmsMessageService;
+import com.dsm.dcs.utils.RandomCodeUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SmsService {
+
+    private final SmsMessageService smsMessageService;
+    private final UserAuthCodeRepository userAuthCodeRepository;
+
+    public void sendMessageAuthCode(PhoneNumberRequest request) {
+        String randomNumber = RandomCodeUtil.generateRandomCode(6);
+        userAuthCodeRepository.save(UserAuthCode.builder()
+                .phoneNumber(request.getPhoneNumber())
+                .code(randomNumber).build());
+        smsMessageService.sendMessage(request.getPhoneNumber(), randomNumber);
+    }
+
+    public void checkCoincideAuthCode(SmsRequest request) {
+        UserAuthCode userAuthCode = userAuthCodeRepository.findById(request.getPhoneNumber())
+                .orElseThrow(() -> PhoneNumberNotFoundException.EXCEPTION);
+        if (userAuthCode.getCode().equals(request.getCode())) {
+            throw PhoneNumberNotMatchException.EXCEPTION;
+        }
+    }
+
+}
